f2636921ff7ce859d5f47c1cdb076287f1214c37
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-11-06 11:13:39 -0300

    log: trying to fix an error with user req

--- src/routes/pool.ts
+++ src/routes/pool.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import ShortUniqueId from "short-unique-id";
 import { z } from "zod";
+
 import { prisma } from "../lib/prisma";
 import { authenticate } from "../plugins/authenticate";
 
@@ -21,7 +22,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
 		const generate = new ShortUniqueId({ length: 6 });
 		const code = String(generate()).toUpperCase();
 
-		let ownerId = null;
+		// let ownerId = null;
 
 		try {
 			await req.jwtVerify();
